68dff89c060d672fabdba8fd7df09cb40cec3351
Steven SU <stevensu78570@gmail.com>
2024-11-12 23:31:09 +0100

    Update to FunctionPHP.php

--- functionPHP.php
+++ functionPHP.php
@@ -1,8 +1,20 @@
 <?php
 
-//Un tableau contenant tous les films de la base de donné
-function listMovies($dbh)
+//Connexion à la base de donné
+function connectDB()
 {
+    $host = 'localhost';
+    $dbname = 'allodonkeycine';
+    $user = 'root';
+    $password = '';
+    return $dbh = new PDO("mysql:host=" . $host . ";dbname=" . $dbname, $user, $password);
+}
+
+
+//Un tableau contenant tous les films de la base de donné
+function listMovies()
+{
+    $dbh = connectDB();
     $sth = $dbh->prepare('SELECT * FROM films');
     $sth->execute();
     $listMovies = $sth->fetchAll();
@@ -10,8 +22,9 @@ function listMovies($dbh)
 }
 
 //Un tableau contenant les films actuellement diffusés
-function currentMovies($dbh)
+function currentMovies()
 {
+    $dbh = connectDB();
     $sth = $dbh->prepare('SELECT * FROM films where diffusion =1');
     $sth->execute();
     $currentMovies = $sth->fetchAll();
@@ -19,8 +32,9 @@ function currentMovies($dbh)
 }
 
 //Permet à un utilisateur de se connecter en donnait un surnom OU un mot de passe, et un mot de passe
-function login($dbh, $user, $password)
+function login($user, $password)
 {
+    $dbh = connectDB();
     $sth = $dbh->prepare('SELECT * FROM utilisateur where (surnom = :surnom or email= :email) and password= :password');
     $sth->bindParam(':surnom', $user);
     $sth->bindParam(':email', $user);
@@ -32,8 +46,9 @@ function login($dbh, $user, $password)
 
 
 //Permet d'enregistrer la reservation d'un utilisateur à une séance
-function registerReservation($dbh, $userLogged, $movieSession)
+function registerReservation($userLogged, $movieSession)
 {
+    $dbh = connectDB();
     $sth = $dbh->prepare('INSERT INTO reservation (ID_user, ID_seance) VALUES (:id_utilisateur, :id_seance)');
     $sth->bindParam(':id_utilisateur', $userLogged['id']);
     $sth->bindParam(':id_seance', $movieSession['id']);
